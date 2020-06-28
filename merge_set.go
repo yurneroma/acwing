7b12215cd6564a52df90d2c3969eec393bf8bc68
lorrainema <lorrainema@tencent.com>
2020-06-28 22:59:40 +0800

    add solution

--- merge_set.go
+++ merge_set.go
@@ -3,24 +3,26 @@ package main
 import "fmt"
 
 const N = 100010
+
 var p [N]int
+
 func main() {
-	n, m:= 0,0
-	fmt.Scanf("%d%d",&n,&m)
-	for i:=0;i<n;i++{
+	n, m := 0, 0
+	fmt.Scanf("%d%d", &n, &m)
+	for i := 0; i < n; i++ {
 		p[i] = i
 	}
-	for ;m>0;m--{
+	for ; m > 0; m-- {
 		opt := ""
-		a, b := 0,0
-		fmt.Scanf("%s%d%d",&opt,&a,&b)
+		a, b := 0, 0
+		fmt.Scanf("%s%d%d", &opt, &a, &b)
 		switch opt {
 		case "M":
 			p[find(a)] = find(b)
 		case "Q":
 			if find(a) == find(b) {
 				fmt.Println("Yes")
-			}else{
+			} else {
 				fmt.Println("No")
 			}
 
@@ -28,12 +30,9 @@ func main() {
 	}
 }
 
-func find(x int)int{
-	if p[x] != x{
+func find(x int) int {
+	if p[x] != x {
 		p[x] = find(p[x])
 	}
 	return p[x]
 }
-
-
-
