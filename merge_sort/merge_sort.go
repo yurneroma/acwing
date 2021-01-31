6b67f4b606629907cb31243735b80bab09741eb0
JamesLeons <mabinzq007@126.com>
2021-01-31 17:33:42 +0800

    feat: 🎸 add merge sort

---
+++ merge_sort/merge_sort.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := 0
+	fmt.Scan(&n)
+	q := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&q[i])
+	}
+
+	tmp := make([]int, n)
+	mergeSort(q, tmp, 0, n-1)
+	for i := 0; i < n; i++ {
+		fmt.Printf("%d ", q[i])
+	}
+
+}
+
+func mergeSort(q, tmp []int, l, r int) {
+	if l >= r {
+		return
+	}
+
+	mid := (l + r) >> 1
+	mergeSort(q, tmp, l, mid)
+	mergeSort(q, tmp, mid+1, r)
+
+	i := l
+	j := mid + 1
+	k := 0
+	for i <= mid && j <= r {
+		if q[i] <= q[j] {
+			tmp[k] = q[i]
+			k++
+			i++
+		} else {
+			tmp[k] = q[j]
+			k++
+			j++
+		}
+	}
+
+	for i <= mid {
+		tmp[k] = q[i]
+		i++
+		k++
+	}
+	for j <= r {
+		tmp[k] = q[j]
+		k++
+		j++
+	}
+
+	i = l
+	j = 0
+	for i <= r {
+		q[i] = tmp[j]
+		j++
+		i++
+	}
+}
