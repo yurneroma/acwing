5486229e87d6f22d4e8960b5cef1f111f9f1be9d
JamesLeons <mabinzq007@126.com>
2021-01-31 23:32:41 +0800

    feat: 🎸 逆序对数量

---
+++ reverse_pair/reverse_pair.go
@@ -0,0 +1,66 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := 0
+	fmt.Scan(&n)
+	q := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&q[i])
+	}
+
+	tmp := make([]int, n)
+	res := mergeSort(q, tmp, 0, n-1)
+	fmt.Println(res)
+
+}
+
+func mergeSort(q, tmp []int, l, r int) int64 {
+	if l >= r {
+		return 0
+	}
+
+	mid := (l + r) >> 1
+	var res int64
+	res += mergeSort(q, tmp, l, mid)
+	res += mergeSort(q, tmp, mid+1, r)
+
+	i := l
+	j := mid + 1
+	k := 0
+	for i <= mid && j <= r {
+		if q[i] <= q[j] {
+			tmp[k] = q[i]
+			k++
+			i++
+		} else {
+			tmp[k] = q[j]
+			k++
+			j++
+			res += int64(mid - i + 1)
+		}
+	}
+
+	for i <= mid {
+		tmp[k] = q[i]
+		k++
+		i++
+
+	}
+	for j <= r {
+		tmp[k] = q[j]
+		k++
+		j++
+	}
+
+	i = l
+	j = 0
+	for i <= r {
+		q[i] = tmp[j]
+		j++
+		i++
+	}
+
+	return res
+}
